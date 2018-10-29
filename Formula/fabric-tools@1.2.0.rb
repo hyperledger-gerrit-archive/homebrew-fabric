3b39f23ac82958c6aef799474d3c53293a96f661
Alejandro Vicente Grabovetsky <sasha@aid.technology>
2018-10-29 17:58:20 +0200

    Upgrade Fabric tools to 1.2 and 1.3
    Change-Id: Ie3f49969de6b05f1fa1709d3a4ec5e87f3b79381
    Signed-off-by: Alejandro Vicente Grabovetsky <sasha@aid.technology>

---
+++ Formula/fabric-tools@1.2.0.rb
@@ -0,0 +1,22 @@
+# Copyright IBM Corp All Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+class FabricTools < Formula
+  desc "Hyperledger Fabric native binaries installer"
+  homepage "https://hyperledger.org/projects/fabric"
+  url "https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric/hyperledger-fabric/darwin-amd64-1.2.0/hyperledger-fabric-darwin-amd64-1.2.0.tar.gz"
+  sha256 "d56c9ce7d7e4262ec0900cfee24873e8512e69b352913344b9aa458a82ea3788"
+
+  def install
+    bin.install "bin/cryptogen"
+    bin.install "bin/configtxgen"
+    bin.install "bin/configtxlator"
+  end
+
+  test do
+    system "#{bin}/configtxgen --help"
+    system "#{bin}/configtxlator --help"
+    system "#{bin}/cryptogen --help"
+  end
+end
